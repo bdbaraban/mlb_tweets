86cbbb585efb59e517bc9bf8b2ccb3786048140a
bdbaraban <375@holbertonschool.com>
2019-05-13 23:14:23 -0700

    Refactor Team to represent all attributes of team lists and implement update methods through Twitter API calls

--- models/team.py
+++ models/team.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""Defines the base Team class."""
+"""Defines the Team subclass."""
+import json
+import models
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+from models.base_model import BaseModel
 
 
-class Team:
-    """Represents the Twitter handles of a 40-man MLB team roster."""
+class Team(BaseModel):
+    """Represents the roster and Twitter list for a given team.
 
-    def __init__(self, **kwargs):
-        """Initialize a new Team.
+    Attributes:
+        name (str): The Team name.
+        url (str): The URL of the Team's 40-man roster.
+        handle (str): The Team Twitter handle.
+        roster (dict): Key/value pairs of players/Twitter handles.
+        list_id (str): The Team's Twitter list ID.
+        embed (str): The embedded link for the Team's Twitter list.
+    """
 
-        Args:
-            **kwargs (any): Key/value pairs of attributes.
-        """
-        for key, value in kwargs.items():
-            if key == "updated_at":
-                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-            setattr(self, key, value)
-
-    def to_dict(self):
-        """Return a dictionary representation of the Team instance."""
-        d = self.__dict__.copy()
-        d["updated_at"] = self.updated_at.isoformat()
-        return d
+    name = ""
+    url = ""
+    handle = ""
+    roster = ""
+    list_id = ""
+    embed = ""
 
     def update_roster(self):
         """Updates the Team roster."""
@@ -31,9 +32,43 @@ class Team:
         soup = BeautifulSoup(page.text, "html.parser")
         self.roster = {}
         for t in soup.find_all("tbody"):
-            for player in t.find_all("a"):
-                page = requests.get("{}{}".format(self.url[:-1], player["href"]))
+            for p in t.find_all("a"):
+                page = requests.get("{}{}".format(self.url[:-1], p["href"]))
                 soup = BeautifulSoup(page.text, "html.parser")
                 handle = soup.find("a", class_="twitter-follow-button")
                 handle = handle["href"].split("/")[-1]
-                self.roster[player.text] = "None" if handle == "mlb" else handle
+                self.roster[p.text] = "None" if handle == "mlb" else handle
+
+    def update_list(self, oauth):
+        """Updates the Team Twitter list.
+
+        Attributes:
+            oauth (OAuth1Session): The Twitter user-auth session.
+        """
+        url = "https://api.twitter.com/1.1/lists/members.json"
+        params = {
+            "list_id": self.list_id,
+            "count": 41
+        }
+        r = oauth.get(url, params=params).json()
+
+        current_members = set()
+        for u in r.get("users"):
+            if u.get("screen_name").lower() != self.handle.lower():
+                current_members.add(u.get("screen_name").lower())
+        updated_members = set()
+        for h in self.roster.values():
+            if h != "None":
+                updated_members.add(h[1:].lower())
+        to_delete = current_members - updated_members
+        to_add = updated_members - current_members
+
+        if len(to_delete) > 0:
+            url = "https://api.twitter.com/1.1/lists/members/destroy_all.json"
+            params["screen_name"] = ",".join(to_delete)
+            oauth.post(url, params=params)
+
+        if len(to_add) > 0:
+            url = "https://api.twitter.com/1.1/lists/members/create_all.json"
+            params["screen_name"] = ",".join(to_add)
+            oauth.post(url, params=params)
